6370fd885ae7a69738f7fa124dd07c95574c13de
James Tan <jamesxtan-codemonkey@users.noreply.github.com>
2026-01-21 10:47:08 -0800

    Fix packaging issue

--- esbuild.js
+++ esbuild.js
@@ -13,7 +13,7 @@ async function main() {
         sourcesContent: false,
         platform: 'node',
         outfile: 'dist/extension.js',
-        external: ['vscode'],
+        external: ['vscode', 'ssh2'],
         logLevel: 'info',
         plugins: [
             /* add plugins here */
